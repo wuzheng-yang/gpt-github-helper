ecc74557336ab4e82183c15c6bd4ac7db99aa4fe
yzw <448719222@qq.com>
2026-06-08 16:46:29 +0800

    1

--- local-server/start_server.bat
+++ local-server/start_server.bat
@@ -3,13 +3,13 @@ chcp 65001 >nul
 echo 正在启动 GPT 本地通知服务...
 echo.
 
-python -m pip install -r requirements.txt
+echo D:\sof\env\anaconda3\envs\ai-gzb\python -m pip install -r requirements.txt
 
 echo.
 echo 服务启动地址：http://127.0.0.1:18888
 echo 健康检查地址：http://127.0.0.1:18888/health
 echo.
 
-python local_notify_server.py
+D:\sof\env\anaconda3\envs\ai-gzb\python local_notify_server.py
 
 pause
